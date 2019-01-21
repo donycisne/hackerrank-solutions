a14a9547c7ecbdabc74c7ee589932c31befd8709
Dony Cisneros <dony.cisne@gmail.com>
2019-01-20 23:46:57 -0300

    10 JS - Day 01

---
+++ 10-days-of-javascript/Day-01/01-hello-world.js
@@ -0,0 +1,18 @@
+/**
+ * Complete the function greeting.
+ * It has one parameter, parameterVariable.
+ * Perform the following tasks to complete this challenge:
+ * 1 - Use console.log() to print Hello, World! on a new line in the console.
+ *     The code for this portion of the task is already provided.
+ * 2 - Use console.log() to print the contents of parameterVariable.
+ */
+
+function greeting(parameterVariable) {
+  // This line prints 'Hello, World!' to the console:
+  console.log('Hello, World!');
+  // Write a line of code that prints parameterVariable to stdout using console.log:
+  console.log(parameterVariable);
+}
+
+// Output
+greeting('Everyone can write code 😊');
