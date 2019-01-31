59606e90556a3f7e29179075a34d69ee553bb844
Dony Cisneros <dony.cisne@gmail.com>
2019-01-31 12:09:36 -0300

    30 Code - Day 01

---
+++ Tutorials/30-Days-of-Code/01-hello-world.py
@@ -0,0 +1,12 @@
+'''
+Task:
+  To complete this challenge print Hello, World. on a single line,
+  and finally print the value of your variable on a second line.
+  You've got this!
+'''
+
+# Solution
+input_string = input('Enter your name: ')
+
+print('➡ Hello, World.')
+print(f'➡ Hello {input_string}.')
