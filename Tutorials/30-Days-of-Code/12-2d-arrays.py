f1e39490fd37387c9270ffee09728e52b8557a5c
Dony Cisneros <dony.cisne@gmail.com>
2019-02-07 19:01:07 -0300

    30 Code - Day 12

---
+++ Tutorials/30-Days-of-Code/12-2d-arrays.py
@@ -0,0 +1,22 @@
+'''
+  Task:
+    Calculate the hourglass sum for every hourglass in A,
+    then print the maximum hourglass sum.
+'''
+
+# Solution
+if __name__ == '__main__':
+  arr = []
+
+  for _ in range(6):
+    arr.append(list(map(int, input().rstrip().split())))
+
+  res = []
+
+  for x in range(0, 4):
+    for y in range(0, 4):
+      s = sum(arr[x][y:y+3]) + arr[x+1][y+1] + sum(arr[x+2][y:y+3])
+      res.append(s)
+
+  print('➡', max(res))
+
